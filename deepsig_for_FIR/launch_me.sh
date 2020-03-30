24abdf77090429299def0e0564e5f566a852c600
salvododo <40605700+salvododo@users.noreply.github.com>
2020-03-30 10:26:08 -0400

    fixed bash script

--- deepsig_for_FIR/launch_me.sh
+++ deepsig_for_FIR/launch_me.sh
@@ -9,7 +9,7 @@ gpu=$1
 # --h5_path /mnt/WDMyBook/bruno/deepsig/GOLD_XYZ_OSC.0001_1024.hdf5 \
 # --data_path /mnt/WDMyBook/bruno/deepsig/GOLD_XYZ_OSC.0001_1024.hdf5 \
 ​
-python ./DeepSig.py \
+python2 ./DeepSig.py \
         --train_cnn True \
         --train_fir False \
         --h5_path /mnt/WDMyBook/bruno/deepsig/GOLD_XYZ_OSC.0001_1024.hdf5 \
