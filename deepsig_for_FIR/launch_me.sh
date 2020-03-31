66153b9f60cf3fac975d5dca0da9cc8c0eeac829
salvododo <40605700+salvododo@users.noreply.github.com>
2020-03-31 16:05:16 -0400

    let's train the fir

--- deepsig_for_FIR/launch_me.sh
+++ deepsig_for_FIR/launch_me.sh
@@ -16,7 +16,7 @@ python2 ./DeepSig.py \
         --data_path /mnt/WDMyBook2/salvo/deepsig/2018.01/GOLD_XYZ_OSC.0001_1024.hdf5 \
         --batch_size 32 \
         --epochs 10 \
-        --fir_size 10 \
+        --fir_size 100 \
 	    --load_indexes \
         --max_steps 0 \
         --num_ex_mod 106496 \
