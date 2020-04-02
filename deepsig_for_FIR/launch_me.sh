96112f7cf1879fee88a5a9a83409bbddd5d4ecbe
salvododo <s.doro@northeastern.edu>
2020-04-02 13:56:24 -0400

    fixed launch script

--- deepsig_for_FIR/launch_me.sh
+++ deepsig_for_FIR/launch_me.sh
@@ -14,13 +14,13 @@ python2 ./DeepSig.py \
         --train_cnn \
         --h5_path /mnt/WDMyBook2/salvo/deepsig/2018.01/GOLD_XYZ_OSC.0001_1024.hdf5 \
         --data_path /mnt/WDMyBook2/salvo/deepsig/2018.01/GOLD_XYZ_OSC.0001_1024.hdf5 \
-        --batch_size 32 \
-        --epochs 10 \
+        --batch_size 64 \
+        --epochs 15 \
         --fir_size 100 \
-        --max_steps 10000 \
+        --max_steps 30000 \
         --num_ex_mod 106496 \
         --num_classes 24 \
         --id_gpu $gpu \
-        --patience 5 \
+        --patience 3 \
 #	> /home/salvo/deepsig_res/out.log \
 #	2> /home/salvo/deepsig_res/err.log
