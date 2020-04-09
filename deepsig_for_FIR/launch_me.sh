653d6b5d4ffcd328c73231e762613c05139949b0
salvododo <s.doro@northeastern.edu>
2020-04-09 18:11:11 -0400

    start 128 batch

--- deepsig_for_FIR/launch_me.sh
+++ deepsig_for_FIR/launch_me.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64/
 gpu=$1
+fir_size=$2
 
 # dgx-2
 # --h5_path /mnt/nas/bruno/deepsig/2018.01/GOLD_XYZ_OSC.0001_1024.hdf5 \
@@ -17,11 +18,11 @@ python2 ./DeepSig.py \
         --data_path /mnt/WDMyBook2/salvo/deepsig/2018.01/GOLD_XYZ_OSC.0001_1024.hdf5 \
         --batch_size 128 \
         --epochs 50 \
-        --fir_size 10 \
+        --fir_size $fir_size \
         --num_ex_mod 106496 \
         --num_classes 24 \
         --id_gpu $gpu \
         --patience 10 \
-	--save_path /home/salvo/deepsig_res/saved_models/per_dev_10
+	--save_path /home/salvo/deepsig_res/saved_models/per_dev_$fir_size
 #	> /home/salvo/deepsig_res/out.log \
 #	2> /home/salvo/deepsig_res/err.log
